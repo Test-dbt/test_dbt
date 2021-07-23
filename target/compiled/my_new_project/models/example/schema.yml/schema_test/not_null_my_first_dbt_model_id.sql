dd8aed74ba4556e97804ab86887d920f01729b68
test.dbt.test@gmail.com <test.dbt.test@gmail.com>
2021-07-23 06:19:49 +0000

    c

--- target/compiled/my_new_project/models/example/schema.yml/schema_test/not_null_my_first_dbt_model_id.sql
+++ target/compiled/my_new_project/models/example/schema.yml/schema_test/not_null_my_first_dbt_model_id.sql
@@ -3,7 +3,7 @@
     
 
 select *
-from DEMO_DB.Atlan_dbt.my_first_dbt_model
+from DEMO_DB.Atlan_dbt_staging.my_first_dbt_model
 where id is null
 
 
--- target/compiled/my_new_project/models/example/schema.yml/schema_test/unique_my_second_dbt_model_id.sql
+++ target/compiled/my_new_project/models/example/schema.yml/schema_test/unique_my_second_dbt_model_id.sql
@@ -6,7 +6,7 @@ select
     id,
     count(*) as n_records
 
-from DEMO_DB.Atlan_dbt.my_second_dbt_model
+from DEMO_DB.Atlan_dbt_staging.my_second_dbt_model
 where id is not null
 group by id
 having count(*) > 1
